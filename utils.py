aade55257de84398eba9a9f6f1a1f47e79439b76
ashishrawathome <ashishrawathome@gmail.com>
2023-11-04 15:13:03 +0530

    Assignment4

--- utils.py
+++ utils.py
@@ -4,6 +4,9 @@ from itertools import product
 from sklearn import tree
 from sklearn.metrics import confusion_matrix
 
+import joblib
+import pickle
+
 # Preprocess data
 def get_preprocess_data(data):
     n_samples = len(data)
@@ -32,11 +35,17 @@ def train_model(x, y, model_params, model_type='svm'):
         
         # Learn the digits on the train subset
         model.fit(x, y)
+
+        # Save model
+        joblib.dump(model, 'digits_train_' + model_type + '_' + str(model_params) + '_' '.joblib')
     
     if model_type == 'tree':
         model = tree.DecisionTreeClassifier(**model_params)
 
         model.fit(x, y)
+
+        # Save model
+        joblib.dump(model, 'digits_train_' + model_type + '_' + str(model_params) + '_' + '.joblib')
     
     return model
 
@@ -58,6 +67,9 @@ def tune_hparams(X_train, Y_train, X_dev, y_dev, list_of_all_param_combination):
         cur_m = train_model(X_train, Y_train, {'gamma': g, 'C':c}, model_type='svm')
         predicted, model_classification, accuracy_score_train, confusion_matrix = predict_and_eval(cur_m, X_train, Y_train)
         predicted, model_classification, accuracy_score_dev, confusion_matrix = predict_and_eval(cur_m, X_dev, y_dev)
+
+        # Save model
+        # joblib.dump(cur_m, 'digits_train_svm_' + '_' + str(list_of_all_param_combination) + '_' '.joblib')
 
         if accuracy_score_dev > best_accuracy:
             best_accuracy = accuracy_score_dev
@@ -85,6 +97,10 @@ def tune_hparams_decision_tree(X_train, Y_train, X_dev, y_dev, params):
 
 def predict_and_eval(model, X_test, y_test):
     predicted = model.predict(X_test)
+
+    # Save model
+    joblib.dump(model, 'digits_predict.joblib')
+
     model_classification = metrics.classification_report(y_test, predicted)
     accuracy_score = metrics.accuracy_score(y_test, predicted)
     precision_score = metrics.precision_score(y_test, predicted, average='macro')
